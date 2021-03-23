a145fd79809f7e024deb10c8159dfb4a44df0a1f
Avantha <avanthac@yahoo.com>
2021-03-24 00:02:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a3de/MainActivity2.java
@@ -0,0 +1,25 @@
+package com.example.a3de;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent i = getIntent();
+
+        editText = findViewById(R.id.input1);
+
+        editText.setText(i.getStringExtra("name1"));
+
+    }
+}
